175b9320c5ef0c152a414ab1bf2c57793488578d
Fernando Ferreira <fernandodof@gmail.com>
2020-05-17 20:12:45 +0200

    set up initial screen and fonts

---
+++ screens/RecipeDetailsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const RecipeDetailsScreen = props => {
+	return (
+		<View style={styles.screen}>
+			<Text>Category Recipe Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignContent: 'center'
+	}
+});
+
+export default RecipeDetailsScreen;
